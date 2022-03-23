9078f4ecc91e2a53a736d6387a42a815a00e2838
gnzlplcs <palaciosgm@outlook.com>
2022-03-23 11:37:05 -0500

    🖼️ se agregó logo de FCC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
     <div className="App">
-    
+      <div className="freecodecamp-logo-contenedor">
+        <img className="freecodecamp-logo" src={freeCodeCampLogo} alt="Logo de freeCodeCamp" />
+      </div>
     </div>
   );
 }
